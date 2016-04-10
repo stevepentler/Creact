8ba22504ccde880e1b137396edfc1b4f94c7df82
Steve Pentler <stevepentler@gmail.com>
2016-04-10 11:46:50 -0600

    Deletes skills

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,11 +1,25 @@
-var AllSkills = React.createClass({
+let AllSkills = React.createClass({
+
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
+  removeSkillFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
+  },
+
   render() {
-    var skills = this.props.skills.map((skill) => {
+    let skills = this.props.skills.map((skill) => {
       return (
         <div key={skill.id}>
           <h3>{skill.name}</h3>
           <p><strong>Level:</strong> {skill.level}</p>
           <p>{skill.details}</p>
+          <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
         </div>
       )
     });
