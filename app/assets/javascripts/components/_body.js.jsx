2b4895009be10134da5ba2f8dd243da45f353c42
Steve Pentler <stevepentler@gmail.com>
2016-04-10 11:28:20 -0600

    Finishes Create

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,13 +1,26 @@
 var Body = React.createClass({
+  getInitialState() {
+      return { skills: [] };
+  },
+
+  componentDidMount() {
+      $.getJSON('/api/v1/skills.json', (response) => {this.setState({ skills: response }) });
+  },
+
+  handleSubmit(skill) {
+    let newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState });
+  },
+
   render() {
     return (
       <div>
-        <Header />
-        <NewSkill />
-        <AllSkills />
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills} />
       </div>
     )
   }
 });
 
 // didn't work without div containing Header and Skills
+// concat adds the skills to the old state
